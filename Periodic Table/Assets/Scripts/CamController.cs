2f6c212a44fdca44f3c824f54e02b16005d6c774
Mehmet Güler <meglr@hotmail.com>
2017-07-25 16:59:28 +0300

    Periodic Table
    Unity Periodic Table

---
+++ Periodic Table/Assets/Scripts/CamController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Zoom();
+    }
+
+    void Move()
+    {
+        var x = transform.position.x + (Input.GetAxis("Horizontal") * Time.deltaTime * 10);
+        var y = transform.position.y  + (Input.GetAxis("Vertical") * Time.deltaTime * 10);
+
+        x = Mathf.Clamp(x, -27, 1);
+        y = Mathf.Clamp(y, -9, 8.5f);
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+
+    void Zoom()
+    {
+        var value = transform.position.z + (Input.GetAxis("Mouse ScrollWheel") * 10);
+        value = Mathf.Clamp(value, -20, -2.5f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, value);
+    }
+}
